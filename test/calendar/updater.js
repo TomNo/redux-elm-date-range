101f8bafff27dc3aec63e27a1b601fa6604d84d4
Tomáš Nováčik <tnovacik@gmail.com>
2016-08-08 07:58:25 +0200

    add simple test for calendar initial model

--- test/calendar/updater.js
+++ test/calendar/updater.js
@@ -1,3 +1,26 @@
-/**
- * Created by tom on 8.8.16.
- */
+import { assert } from 'chai';
+
+import updater from '../../src/calendar/updater';
+import moment from 'moment';
+
+describe('Calendar initial model', () => {
+    it('should return initial model with current date', () => {
+        var date = new Date();
+        var day = date.getDate();
+        var month = date.getMonth();
+        var year = date.getFullYear();
+        var expectedModel = {sDay: day,
+                             sYear: year,
+                             sMonth: month,
+                             bMonth: month,
+                             bYear: year};
+        assert.deepEqual(updater(), expectedModel);
+    });
+
+    // it('should set greeted flag when clicking SayHi', () => {
+    //     let model = undefined;
+    //     model = updater();
+    //     model = updater(model, { type: 'SayHi' });
+    //     assert.deepEqual(model, { greeted: true });
+    // });
+});
