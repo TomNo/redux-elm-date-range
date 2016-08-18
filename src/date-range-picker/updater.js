6be90ae6c4eb95d37eef1765edc4efa25ceb59d9
Tomáš Nováčik <tnovacik@gmail.com>
2016-08-18 14:24:46 +0200

    add check that range values are selected correctly

--- src/date-range-picker/updater.js
+++ src/date-range-picker/updater.js
@@ -1,4 +1,4 @@
-import {Updater} from 'redux-elm';
+import {Updater, Matchers} from 'redux-elm';
 import {createSelector} from 'reselect';
 
 import calendarUpdater, {
@@ -10,15 +10,72 @@ const TO_INPUT_NAME = "toDate";
 
 export const initialModel = ((fromDate = FROM_INPUT_NAME,
                               toDate = TO_INPUT_NAME) => {
-    return {
+    var model = {
         from: calendarViewerInit(fromDate),
-        to: {...calendarViewerInit(toDate)}
+        to: {...calendarViewerInit(toDate)},
+        rangeError: false
+    };
+
+    return {
+        ...model, previousFromSelection: model,
+        previousToSelection: model
     };
 });
+
+
+const createDateObject = (({selectedDay, selectedYear, selectedMonth}) => {
+    if (selectedDay && selectedYear && selectedMonth) {
+        return new Date(selectedYear, selectedMonth, selectedDay);
+    }
+    else {
+        return null;
+    }
+
+});
+
+/**
+ * Copy & paste from http://salsita.github.io/redux-elm/custom-matchers/writing-custom-matcher.html
+ */
+const endsWithMatcher = pattern => {
+    return action => {
+        if (action.type.endsWith(`.${pattern}`)) {
+            const wrapRegExp = new RegExp(`(.*)\.${pattern}$`);
+
+            return {
+                id: `ends-with${pattern}`,
+                unwrappedType: pattern,
+                wrap: type => `${action.type.match(wrapRegExp)[1]}.${type}`,
+                args: {}
+            }
+        } else {
+            return false;
+        }
+    };
+};
 
 export default new Updater(initialModel())
     .case('From', (model, action) =>
         ({...model, from: calendarUpdater(model.from, action)}))
     .case('To', (model, action) =>
         ({...model, to: calendarUpdater(model.to, action)}))
+    .case('DateChanged', (model) => {
+        var fromDate = createDateObject(model.from);
+        var toDate = createDateObject(model.to);
+        // negative range check
+        if (fromDate && toDate && (fromDate - toDate) >= 0) {
+            model = {
+                ...model,
+                rangeError: true,
+                from: model.previousFromSelection,
+                to: model.previousToSelection
+            };
+        } else {
+            model = {
+                ...model,
+                previousFromSelection: model.from,
+                previousToSelection: model.to
+            };
+        }
+        return model;
+    }, endsWithMatcher)
     .toReducer();
--- src/date-range-picker/view.js
+++ src/date-range-picker/view.js
@@ -3,11 +3,19 @@ import {view, forwardTo} from 'redux-elm';
 
 import CalendarViewer from '../calendar/view';
 
+const renderRangeError = ((errorPresent) => {
+    return errorPresent ? "Error:From date must be sooner then to date." : "";
+});
+
 export default view(({model, dispatch}) => {
-    return <span>
-        <CalendarViewer model={model.from}
-                        dispatch={forwardTo(dispatch, 'From')}/>
-        -
-        <CalendarViewer model={model.to} dispatch={forwardTo(dispatch, 'To')}/>
-    </span>;
+    return <div>
+        <span> {renderRangeError(model.rangeError)} </span>
+        <span>
+            <CalendarViewer model={model.from}
+                            dispatch={forwardTo(dispatch, 'From')}/>
+            -
+            <CalendarViewer model={model.to}
+                            dispatch={forwardTo(dispatch, 'To')}/>
+        </span>
+    </div>;
 });
