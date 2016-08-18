e7a307dc77c77496ae1a8bcdbec7bd0bad05f065
Tomáš Nováčik <tnovacik@gmail.com>
2016-08-18 15:09:54 +0200

    add date range picker tests

--- test/date-range-picker/updater.js
+++ test/date-range-picker/updater.js
@@ -1,3 +1,45 @@
-/**
- * Created by tom on 18.8.16.
- */
+import {assert} from 'chai';
+
+import updater from '../../src/date-range-picker/updater';
+
+describe('Date range picker', () => {
+    var fromDate = {
+        selectedDay: 20,
+        selectedYear: 2015,
+        selectedMonth: 10};
+    fromDate.browsingYear = fromDate.selectedYear;
+    fromDate.browsingMonth = fromDate.selectedMonth;
+
+    var toDate = {...fromDate};
+    var model = {
+        from: fromDate,
+        to: toDate,
+        previousFromSelection: fromDate,
+        previousToSelection: toDate,
+        rangeError: false
+    };
+
+    it('should not set to date sooner than from date.',
+        () => {
+            var testModel = {...model};
+            testModel.toDate = testModel.fromDate - 1;
+            var expectedModel = {...testModel, rangeError: true};
+            var actualModel = updater(testModel, {
+                type: "To.DateChanged",
+                selectedDay: fromDate.selectedDay - 1
+            });
+            assert.deepEqual(actualModel, expectedModel);
+        });
+
+    it('should not set from date later than to date.',
+        () => {
+            var testModel = {...model};
+            var expectedModel = {...testModel, rangeError: true};
+            var actualModel = updater(testModel, {
+                type: "From.DateChanged",
+                selectedDay: toDate.selectedDay + 1
+            });
+            assert.deepEqual(actualModel, expectedModel);
+        });
+});
+
